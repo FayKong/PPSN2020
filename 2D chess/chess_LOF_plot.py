ec5c896e3fccc2408c0d810be4cbc566ee44a9fb
FayKong <67231428+FayKong@users.noreply.github.com>
2022-02-22 13:42:19 +0100

    Update chess_LOF_plot.py

--- 2D chess/chess_LOF_plot.py
+++ 2D chess/chess_LOF_plot.py
@@ -1,69 +1,176 @@
-# %% experiment with LOF score
+# %% figure
+f=open('chess'+str(div)+'x'+str(div)+'_n'+str(N)+'_'+str(per)+'.txt','w')
 
-clf = LocalOutlierFactor(n_neighbors=10, contamination=0.1)
-# use fit_predict to compute the predicted labels of the training samples
-# (when LOF is used for outlier detection, the estimator has no predict,
-# decision_function and score_samples methods).
-y_pred = clf.fit_predict(X)
-n_errors = (y_pred != y).sum()
-X_scores = clf.negative_outlier_factor_
-Score_qu = np.quantile(X_scores, 0.12)  # find the quantile of outlier score (the least 12%)
+for xw, yw  in zip(X,y):
+	f.write(str(xw[0])+" "+str(xw[1])+" "+str(float(yw))+"\n")
+
 
 n_classes = 2
-plot_colors = ['#000000', '#FFFFFF']
+plot_colors = ['#000000','#FFFFFF']
 plot_step = 0.02
 
-cmap_blw = ListedColormap(['#000000', '#FFFFFF'])
-cmap_blw_light = ListedColormap(['#6E6E6E', '#FFFFFF'])
-
-# plot with all the outlier score (showing as radius)
+cmap_blw = ListedColormap(['#000000','#FFFFFF'])
+cmap_blw_light = ListedColormap(['#6E6E6E','#FFFFFF'])
 
 plt.axis("tight")
+
 # Plot the training points
 for i, color in zip([0.,1.], plot_colors):
-    idx = np.where(y == i)
-    plt.scatter(X[idx, 0], X[idx, 1], s=20, c=color, cmap=cmap_blw, edgecolors='black')
+    idx = np.where(y[train] == i)
+    plt.scatter(X[train][idx, 0], X[train][idx, 1], s=20, c=color, cmap=cmap_blw, edgecolors='black')
 
-radius = (X_scores.max() - X_scores) / (X_scores.max() - X_scores.min())
-#plt.scatter(X[i, 0], X[i, 1], s=1000 * radius, edgecolors='r', facecolors='none')
-plt.scatter(X[:, 0], X[:, 1], s=1000 * radius, edgecolors='r', facecolors='none')
 
-plt.title('Chess'+str(div)+'x'+str(div)+' Imbalanced data-set')
+plot_colorss = ['#569597','#a71368']
+for i, color in zip([0., 1.], plot_colorss):
+    idxx = np.where(y[test] == i)
+    plt.scatter(X[test][idxx, 0], X[test][idxx, 1], s=20, c=color, cmap=cmap_blw, edgecolors=color)
+
+error = list(np.where(y[test]!=y_test_pred))
+#error[0][1]
+
+for i in range(0, len(error[0])):
+    plt.scatter(X[test][i, 0], X[test][i, 1], s=100, edgecolors='r', facecolors='none')
+
+
+plt.title('Chess Classification Performance (Add = NO)')
 plt.legend()
 #plt.savefig('plots/chess'+str(div)+'x'+str(div)+'_n'+str(N)+'_'+str(per)+'DT.eps')
 plt.show()
 
-# %% plot with leass-than-quantile outlier score (showing as radius)
-
-X_scores_1 = np.array([X_scores[x:x+1] for x in range(0, len(X_scores), 1)])
-X_all['LOF'] = X_scores_1
-radius = (X_scores.max() - X_scores) / (X_scores.max() - X_scores.min())
-radius_1 = np.array([radius[x:x+1] for x in range(0, len(radius), 1)])
-X_all['radius'] = radius_1
-LOF_sort = X_all.sort_values(by=['LOF'])
-maj_safe_idx = LOF_sort[(LOF_sort['degree'] == 1) & (LOF_sort['Class'] == 0)].index
-X_drop_safe_maj = LOF_sort.drop(index=maj_safe_idx)
-
+# %% original
 
 plt.axis("tight")
-# Plot the training points
-for i, color in zip([0.,1.], plot_colors):
-    idx = np.where(y == i)
-    plt.scatter(X[idx, 0], X[idx, 1], s=20, c=color, cmap=cmap_blw, edgecolors='black')
 
-# for i in range(0, LOF_sort.shape[0]):
-    # if X_scores[i] < Score_qu:
-        # if X_all.iloc[i, 3] != 1:
-                # plt.scatter(X[i, 0], X[i, 1], s=1000 * radius[i], edgecolors='r', facecolors='none')
+idx = np.where(y[train] == 0)
+plt.scatter(X[train][idx, 0], X[train][idx, 1], s=20, c=plot_colors[0], cmap=cmap_blw, edgecolors='black')
+idx1 = np.where(y[train] == 1)
+plt.scatter(X[train][idx1, 0], X[train][idx1, 1], s=20, c=plot_colors[1], cmap=cmap_blw, edgecolors='black')
 
-t = 0
-for i in range(0, X_drop_safe_maj.shape[0]):
-    if t < 0.12*LOF_sort.shape[0]:  # # 0.12 should be defined
-        plt.scatter(X_drop_safe_maj.iloc[i, 0], X_drop_safe_maj.iloc[i, 1],
-                    s=1000 * X_drop_safe_maj.iloc[i, 5], edgecolors='r', facecolors='none')
-        t = t + 1
+plot_colorss = ['#569597', '#a71368']
 
-plt.title('Chess'+str(div)+'x'+str(div)+' Class-imbalance Classification with edited LOF score')
-plt.legend()
-#plt.savefig('plots/chess'+str(div)+'x'+str(div)+'_n'+str(N)+'_'+str(per)+'DT.eps')
+idxx = np.where(y[test] == 0)
+plt.scatter(X[test][idxx, 0], X[test][idxx, 1], s=20, marker = '^', c=plot_colorss[0], cmap=cmap_blw, edgecolors=color)
+idxx1 = np.where(y[test] == 1)
+plt.scatter(X[test][idxx1, 0], X[test][idxx1, 1], s=20, marker = '*', c=plot_colorss[1], cmap=cmap_blw, edgecolors=color)
+
+error = list(np.where(y[test] != y_test_pred))
+# error[0][1]
+
+
+type1 = plt.scatter(X[train][idx, 0], X[train][idx, 1], s=20, c=plot_colors[0], cmap=cmap_blw, edgecolors='black')
+type2 = plt.scatter(X[train][idx1, 0], X[train][idx1, 1], s=20, c=plot_colors[1], cmap=cmap_blw, edgecolors='black')
+type3 = plt.scatter(X[test][idxx, 0], X[test][idxx, 1], s=20, marker = '^', c=plot_colorss[0], cmap=cmap_blw, edgecolors=plot_colorss[0])
+type4 = plt.scatter(X[test][idxx1, 0], X[test][idxx1, 1], s=20, marker = '*', c=plot_colorss[1], cmap=cmap_blw, edgecolors=plot_colorss[1])
+
+for i in range(0, len(error[0])):
+    plt.scatter(X[test][i, 0], X[test][i, 1], s=100, edgecolors='r', facecolors='none')
+
+plt.ylim((-0.1, 1.2))
+plt.title('Chess Classification Performance (Sampler = NO, Add = NO)')
+plt.legend((type1, type2, type3, type4), ("majority training", "minority training", "majority test","minority test"), loc = 0, fontsize = 'medium', framealpha = 0.2)
+# plt.savefig('plots/chess'+str(div)+'x'+str(div)+'_n'+str(N)+'_'+str(per)+'DT.eps')
+plt.show()
+
+
+# %% add
+
+plt.axis("tight")
+
+idx = np.where(y[train] == 0)
+plt.scatter(X[train][idx, 0], X[train][idx, 1], s=20, c=plot_colors[0], cmap=cmap_blw, edgecolors='black')
+idx1 = np.where(y[train] == 1)
+plt.scatter(X[train][idx1, 0], X[train][idx1, 1], s=20, c=plot_colors[1], cmap=cmap_blw, edgecolors='black')
+
+plot_colorss = ['#569597', '#a71368']
+
+idxx = np.where(y[test] == 0)
+plt.scatter(X[test][idxx, 0], X[test][idxx, 1], s=20, marker = '^', c=plot_colorss[0], cmap=cmap_blw, edgecolors=color)
+idxx1 = np.where(y[test] == 1)
+plt.scatter(X[test][idxx1, 0], X[test][idxx1, 1], s=20, marker = '*', c=plot_colorss[1], cmap=cmap_blw, edgecolors=color)
+
+error = list(np.where(y[test] != y_test_pred))
+# error[0][1]
+
+
+type1 = plt.scatter(X[train][idx, 0], X[train][idx, 1], s=20, c=plot_colors[0], cmap=cmap_blw, edgecolors='black')
+type2 = plt.scatter(X[train][idx1, 0], X[train][idx1, 1], s=20, c=plot_colors[1], cmap=cmap_blw, edgecolors='black')
+type3 = plt.scatter(X[test][idxx, 0], X[test][idxx, 1], s=20, marker = '^', c=plot_colorss[0], cmap=cmap_blw, edgecolors=plot_colorss[0])
+type4 = plt.scatter(X[test][idxx1, 0], X[test][idxx1, 1], s=20, marker = '*', c=plot_colorss[1], cmap=cmap_blw, edgecolors=plot_colorss[1])
+
+for i in range(0, len(error[0])):
+    plt.scatter(X[test][i, 0], X[test][i, 1], s=100, edgecolors='r', facecolors='none')
+
+plt.ylim((-0.1, 1.2))
+plt.title('Chess Classification Performance (Sampler = NO, Add = YES)')
+plt.legend((type1, type2, type3, type4), ("majority training", "minority training", "majority test","minority test"), loc = 0, fontsize = 'medium', framealpha = 0.2)
+# plt.savefig('plots/chess'+str(div)+'x'+str(div)+'_n'+str(N)+'_'+str(per)+'DT.eps')
+plt.show()
+
+
+# %% original SMOTE
+
+plt.axis("tight")
+
+idx = np.where(yr == 0)
+plt.scatter(Xr[idx, 0], Xr[idx, 1], s=20, c=plot_colors[0], cmap=cmap_blw, edgecolors='black')
+idx1 = np.where(yr == 1)
+plt.scatter(Xr[idx1, 0], Xr[idx1, 1], s=20, c=plot_colors[1], cmap=cmap_blw, edgecolors='black')
+
+plot_colorss = ['#569597', '#a71368']
+
+idxx = np.where(y[test] == 0)
+plt.scatter(X[test][idxx, 0], X[test][idxx, 1], s=20, marker = '^', c=plot_colorss[0], cmap=cmap_blw, edgecolors=color)
+idxx1 = np.where(y[test] == 1)
+plt.scatter(X[test][idxx1, 0], X[test][idxx1, 1], s=20, marker = '*', c=plot_colorss[1], cmap=cmap_blw, edgecolors=color)
+
+error = list(np.where(y[test] != y_test_pred))
+# error[0][1]
+
+
+type1 = plt.scatter(Xr[idx, 0], Xr[idx, 1], s=20, c=plot_colors[0], cmap=cmap_blw, edgecolors='black')
+type2 = plt.scatter(Xr[idx1, 0], Xr[idx1, 1], s=20, c=plot_colors[1], cmap=cmap_blw, edgecolors='black')
+type3 = plt.scatter(X[test][idxx, 0], X[test][idxx, 1], s=20, marker = '^', c=plot_colorss[0], cmap=cmap_blw, edgecolors=plot_colorss[0])
+type4 = plt.scatter(X[test][idxx1, 0], X[test][idxx1, 1], s=20, marker = '*', c=plot_colorss[1], cmap=cmap_blw, edgecolors=plot_colorss[1])
+
+for i in range(0, len(error[0])):
+    plt.scatter(X[test][i, 0], X[test][i, 1], s=100, edgecolors='r', facecolors='none')
+
+plt.ylim((-0.1, 1.2))
+plt.title('Chess Classification Performance (Sampler = SMOTE, Add = NO)')
+plt.legend((type1, type2, type3, type4), ("majority training", "minority training", "majority test","minority test"), loc = 0, fontsize = 'medium', framealpha = 0.2)
+# plt.savefig('plots/chess'+str(div)+'x'+str(div)+'_n'+str(N)+'_'+str(per)+'DT.eps')
+plt.show()
+
+
+# %% add SMOTE
+
+plt.axis("tight")
+
+idx = np.where(yr == 0)
+plt.scatter(Xr[idx, 0], Xr[idx, 1], s=20, c=plot_colors[0], cmap=cmap_blw, edgecolors='black')
+idx1 = np.where(yr == 1)
+plt.scatter(Xr[idx1, 0], Xr[idx1, 1], s=20, c=plot_colors[1], cmap=cmap_blw, edgecolors='black')
+
+plot_colorss = ['#569597', '#a71368']
+
+idxx = np.where(y[test] == 0)
+plt.scatter(X[test][idxx, 0], X[test][idxx, 1], s=20, marker = '^', c=plot_colorss[0], cmap=cmap_blw, edgecolors=color)
+idxx1 = np.where(y[test] == 1)
+plt.scatter(X[test][idxx1, 0], X[test][idxx1, 1], s=20, marker = '*', c=plot_colorss[1], cmap=cmap_blw, edgecolors=color)
+
+error = list(np.where(y[test] != y_test_pred))
+# error[0][1]
+
+type1 = plt.scatter(Xr[idx, 0], Xr[idx, 1], s=20, c=plot_colors[0], cmap=cmap_blw, edgecolors='black')
+type2 = plt.scatter(Xr[idx1, 0], Xr[idx1, 1], s=20, c=plot_colors[1], cmap=cmap_blw, edgecolors='black')
+type3 = plt.scatter(X[test][idxx, 0], X[test][idxx, 1], s=20, marker = '^', c=plot_colorss[0], cmap=cmap_blw, edgecolors=plot_colorss[0])
+type4 = plt.scatter(X[test][idxx1, 0], X[test][idxx1, 1], s=20, marker = '*', c=plot_colorss[1], cmap=cmap_blw, edgecolors=plot_colorss[1])
+
+for i in range(0, len(error[0])):
+    plt.scatter(X[test][i, 0], X[test][i, 1], s=100, edgecolors='r', facecolors='none')
+
+plt.ylim((-0.1, 1.2))
+plt.title('Chess Classification Performance (Sampler = SMOTE, Add = YES)')
+plt.legend((type1, type2, type3, type4), ("majority training", "minority training", "majority test","minority test"), loc = 0, fontsize = 'medium', framealpha = 0.2)
+# plt.savefig('plots/chess'+str(div)+'x'+str(div)+'_n'+str(N)+'_'+str(per)+'DT.eps')
 plt.show()
